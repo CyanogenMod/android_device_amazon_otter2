2010edb7d895b224b9a1844226f9461642ad91f2
Hashcode <hashcode0f@gmail.com>
2013-02-04 00:21:29 -0800

    fix MAC addr set during boot
    Change-Id: I9041c4771ba1b29b09fc61f45f269fb1e9a2eddf

--- prebuilt/bin/fix-mac.sh
+++ prebuilt/bin/fix-mac.sh
@@ -7,7 +7,7 @@ NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs.bin
 if bbx [ ! -f "$NVS_BIN" ]; then
     bbx mount -o remount,rw /system
     bbx cp ${ORIG_NVS_BIN} ${NVS_BIN}
-#    calibrator set nvs_mac $NVS_BIN $(idme ?| bbx grep 0x0030 | bbx cut -c35-46| bbx sed -e 's/)//g'| bbx sed -e 's/-//g'| bbx sed 's/../&:/g;s/:$//')
+    calibrator set nvs_mac $NVS_BIN $(/system/bin/idme print| bbx grep "mac: " | bbx cut -c6-17| bbx sed -e 's/)//g'| bbx sed -e 's/-//g'| bbx sed 's/../&:/g;s/:$//')
     bbx mount -o remount,ro /system
 fi
 
