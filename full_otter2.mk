227eef1ce5f07759dbfe11d0e045cc0a11c5f239
Hashcode <hashcode0f@gmail.com>
2013-08-15 16:27:30 -0700

    reverse product build order to fix prop set order
    Change-Id: Id80453ae03634dfa9ce44cd194d9a0b52a5a3f10

--- full_otter2.mk
+++ full_otter2.mk
@@ -25,8 +25,8 @@ PRODUCT_PACKAGES += \
         librs_jni
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/amazon/otter2/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 PRODUCT_NAME := full_otter2
 PRODUCT_DEVICE := otter2
