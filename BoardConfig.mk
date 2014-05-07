19d747a34a1c3fc9042d7821f10e19029e270502
Hashcode <hashcode0f@gmail.com>
2014-05-07 13:12:03 -0700

    otter2: use unified releasetools
    Change-Id: Iab3556c9fce4e942a7f40622593e4a2cfac76b9f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,8 +37,8 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 929038336
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_FOLDER)/boot.mk
 
 # hack the ota
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./$(DEVICE_FOLDER)/releasetools/bowser_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./$(DEVICE_FOLDER)/releasetools/bowser_img_from_target_files
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./$(COMMON_FOLDER)/releasetools/bowser_ota_from_target_files
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./$(COMMON_FOLDER)/releasetools/bowser_img_from_target_files
 
 # Recovery
 TARGET_RECOVERY_FSTAB = $(DEVICE_FOLDER)/fstab.otter2
